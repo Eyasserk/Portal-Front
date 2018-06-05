374d1d2641183d791b69cdf396a0357664a5ac25
KANTOUR <x103007@es.produban.com>
2018-06-05 10:46:28 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,66 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var partials = require ('express-partials');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var logger = require('morgan');
+
+var router = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  name: 'sessionID',
+  secret: 'melon',
+  cookie: { secure: false,
+            httpOnly: false,
+            path: '/',
+            expires: new Date( Date.now() + 60 * 60 * 1000 )
+          }
+  })
+);
+
+
+// Helpers dinamicos:
+app.use(function(req, res, next) {
+
+  // si no existe lo inicializa
+  if (!req.session.redir) {
+    req.session.redir = '/';
+  }
+  // Hacer visible req.session en las vistas
+  res.locals.session = req.session;
+  next();
+});
+
+app.use(partials());
+
+app.use('/', router);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
